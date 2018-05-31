369b498b141ff96fcae0c6f797db5928c6bbb135
karthisanjaykk <39474897+karthisanjaykk@users.noreply.github.com>
2018-05-31 18:39:32 +0530

    Add files via upload

---
+++ datatype/src/datatype/IsMultiple.java
@@ -0,0 +1,11 @@
+package datatype;
+
+public class IsMultiple {
+	public static void main(String[]args) {
+		
+	}
+	public boolean Multiple(int a,int b) {
+		return b %a == 0? true : false;
+	}
+
+}
