c8a4ed452b2284824bd75d12744846dfcc83dd63
karthisanjaykk <39474897+karthisanjaykk@users.noreply.github.com>
2018-05-31 18:39:06 +0530

    Add files via upload

---
+++ datatype/src/datatype/ifelse.java
@@ -0,0 +1,13 @@
+package datatype;
+
+public class ifelse {
+	public static void main(String[]args) {
+		int num =20;
+		if(num<30) {
+			System.out.println(30);
+		}else if (num>10){
+			System.out.println(10);
+		}
+	}
+
+}
